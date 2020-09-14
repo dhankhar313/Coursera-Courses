de7801b169c494d94e2f320dd1e162a86235b0db
Rahul Dhankhar <dhankharrahul13@gmail.com>
2020-09-14 17:12:51 +0530

    Added Week 2

--- Data-Structures-and-Algorithms-Specialization/1. Algorithmic Toolbox/Week 2/1_fibonacci_number/fibonacci.cpp
+++ Data-Structures-and-Algorithms-Specialization/1. Algorithmic Toolbox/Week 2/1_fibonacci_number/fibonacci.cpp
@@ -2,28 +2,14 @@
 #include <cassert>
 #include <vector>
 using namespace std;
-// The following code calls a naive algorithm for computing a Fibonacci number.
-//
-// What to do:
-// 1. Compile the following code and run it on an input "40" to check that it is slow.
-//    You may also want to submit it to the grader to ensure that it gets the "time limit exceeded" message.
-// 2. Implement the fibonacci_fast procedure.
-// 3. Remove the line that prints the result of the naive algorithm, comment the lines reading the input,
-//    uncomment the line with a call to test_solution, compile the program, and run it.
-//    This will ensure that your efficient algorithm returns the same as the naive one for small values of n.
-// 4. If test_solution() reveals a bug in your implementation, debug it, fix it, and repeat step 3.
-// 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
-//    and submit it to the grader.
-
 int fibonacci_naive(int n) {
     if (n <= 1)
         return n;
-
-    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+  return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
 }
 
-int fibonacci_fast(int n) {
-    vector<int> f(n);
+long fibonacci_fast(int n) {
+    vector<long> f(n + 1);
     f[0] = 0;
     f[1] = 1;
     for (int i = 2; i <= n; i++){
@@ -42,9 +28,14 @@ void test_solution() {
 int main() {
     int n;
     cin >> n;
-
     // cout << fibonacci_naive(n) << '\n';
     // test_solution();
-    cout << fibonacci_fast(n) << '\n';
+    if (n <= 1){
+        cout << n;
+    }
+    else
+    {
+        cout << fibonacci_fast(n) << '\n';
+    }
     return 0;
 }
