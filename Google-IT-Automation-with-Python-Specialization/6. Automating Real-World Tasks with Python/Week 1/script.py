0a0944ac381ac332e329682229f1f9c519d961b0
Rahul <dhankharrahul13@gmail.com>
2020-10-17 16:39:36 +0530

    Added code

---
+++ Google-IT-Automation-with-Python-Specialization/6. Automating Real-World Tasks with Python/Week 1/script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from PIL import Image
+from glob import glob
+import os
+
+for file in glob('ic_*'):
+    image = Image.open(file).convert('RGB')
+    path, filename = os.path.split(file)
+    filename = os.path.splitext(filename)[0]  # get filename without extension
+    image.rotate(270).resize((128, 128)).save('/opt/icons/{}.jpeg'.format(filena$
+
+print('DONE!!')
