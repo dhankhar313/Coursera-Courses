9a3ef45ae95b52d203ccb6086c4af0893c7a215a
Rahul <dhankharrahul13@gmail.com>
2020-10-17 13:43:30 +0530

    cpp In progress

--- Data-Structures-and-Algorithms-Specialization/1. Algorithmic Toolbox/Week 3/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ Data-Structures-and-Algorithms-Specialization/1. Algorithmic Toolbox/Week 3/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,4 +1,36 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+// def knapsack(dict, capacity):
+//     amount = 0
+//     temp = []
+//     for i, j in dict:
+//         temp_value, temp_weight = j
+//         if temp_weight <= capacity and i not in temp and capacity != 0:
+//             amount += temp_value
+//             capacity -= temp_weight
+//         elif temp_weight > capacity and i not in temp and capacity != 0:
+//             amount += temp_value / (temp_weight / capacity)
+//         else:
+//             pass
+//         temp.append(i)
+//     return format(amount, '.4f')
+
 int main()
 {
-    int items, capacity;
+    int items, capacity, temp1, temp2;
+    cin >> items >> capacity;
+    cout << items << capacity << endl;
+    vector<int> data(items);
+    for (int i = 0; i < items; i++)
+    {
+        cin >> temp1 >> temp2;
+        data.push_back(temp1);
+        data.push_back(temp2);
+    }
+    for (int i = 0; i < data.size(); i++)
+    {
+        cout << data[i] << endl;
+    }
 }
