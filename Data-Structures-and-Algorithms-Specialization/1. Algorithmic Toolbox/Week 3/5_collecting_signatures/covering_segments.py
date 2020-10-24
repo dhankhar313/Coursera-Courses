7dc34ff32683b5a4b34c3f6ccbdc8aabd6d71749
Rahul <dhankharrahul13@gmail.com>
2020-10-24 10:01:20 +0530

    Added py code

--- Data-Structures-and-Algorithms-Specialization/1. Algorithmic Toolbox/Week 3/5_collecting_signatures/covering_segments.py
+++ Data-Structures-and-Algorithms-Specialization/1. Algorithmic Toolbox/Week 3/5_collecting_signatures/covering_segments.py
@@ -1,14 +1,23 @@
-def signatures(coordinates):
-    data = []
-    temp = [tuple(x) for x in coordinates]
-    temp.sort()
-    print(temp)
-    for (i, j) in temp:
-        data.append(tuple(m for m in range(i, j+1)))
-    return data
+# noinspection PyShadowingNames
+def signatures(lst, n):
+    index = 0
+    coordinates = []
+
+    while index < n:
+        curr = lst[index]
+        while index < n - 1 and curr[1] >= lst[index + 1][0]:
+            index += 1
+        coordinates.append(curr[1])
+        index += 1
+    print(len(coordinates))
+    return ' '.join([str(i) for i in coordinates])
 
 
 if __name__ == "__main__":
     num = int(input())
-    coordinates = [map(int, input().split()) for i in range(num)]
-    print(signatures(coordinates))
+    coordinates = []
+    for temp in range(num):
+        x, y = input().split()
+        coordinates.append((x, y))
+    coordinates.sort(key=lambda x: x[1])
+    print(signatures(coordinates, num))
